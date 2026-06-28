192fd43c60cd9ec274915c1ad621c0d30b306cdd
kortiz <kevin.ortiz.st@gmail.com>
2026-06-28 18:50:17 -0500

    Actualiza partidos de octavos

---
+++ src/data/matches_round16.js
@@ -0,0 +1,137 @@
+export const matches = [
+  {
+    id: 1,
+    date: "2026-06-29",
+    home: "Brasil",
+    homeCode: "br",
+    away: "Japón",
+    awayCode: "jp",
+    stadium: "Por confirmar"
+  },
+  {
+    id: 2,
+    date: "2026-06-29",
+    home: "Alemania",
+    homeCode: "de",
+    away: "Paraguay",
+    awayCode: "py",
+    stadium: "Por confirmar"
+  },
+  {
+    id: 3,
+    date: "2026-06-29",
+    home: "Países Bajos",
+    homeCode: "nl",
+    away: "Marruecos",
+    awayCode: "ma",
+    stadium: "Por confirmar"
+  },
+  {
+    id: 4,
+    date: "2026-06-30",
+    home: "Costa de Marfil",
+    homeCode: "ci",
+    away: "Noruega",
+    awayCode: "no",
+    stadium: "Por confirmar"
+  },
+  {
+    id: 5,
+    date: "2026-06-30",
+    home: "Francia",
+    homeCode: "fr",
+    away: "Suecia",
+    awayCode: "se",
+    stadium: "Por confirmar"
+  },
+  {
+    id: 6,
+    date: "2026-06-30",
+    home: "México",
+    homeCode: "mx",
+    away: "Ecuador",
+    awayCode: "ec",
+    stadium: "Por confirmar"
+  },
+  {
+    id: 7,
+    date: "2026-07-01",
+    home: "Inglaterra",
+    homeCode: "gb",
+    away: "RD Congo",
+    awayCode: "cd",
+    stadium: "Por confirmar"
+  },
+  {
+    id: 8,
+    date: "2026-07-01",
+    home: "Bélgica",
+    homeCode: "be",
+    away: "Senegal",
+    awayCode: "sn",
+    stadium: "Por confirmar"
+  },
+  {
+    id: 9,
+    date: "2026-07-01",
+    home: "Estados Unidos",
+    homeCode: "us",
+    away: "Bosnia y Herzegovina",
+    awayCode: "ba",
+    stadium: "Por confirmar"
+  },
+  {
+    id: 10,
+    date: "2026-07-02",
+    home: "España",
+    homeCode: "es",
+    away: "Austria",
+    awayCode: "at",
+    stadium: "Por confirmar"
+  },
+  {
+    id: 11,
+    date: "2026-07-02",
+    home: "Portugal",
+    homeCode: "pt",
+    away: "Croacia",
+    awayCode: "hr",
+    stadium: "Por confirmar"
+  },
+  {
+    id: 12,
+    date: "2026-07-02",
+    home: "Australia",
+    homeCode: "au",
+    away: "Egipto",
+    awayCode: "eg",
+    stadium: "Por confirmar"
+  },
+  {
+    id: 13,
+    date: "2026-07-03",
+    home: "Suiza",
+    homeCode: "ch",
+    away: "Argelia",
+    awayCode: "dz",
+    stadium: "Por confirmar"
+  },
+  {
+    id: 14,
+    date: "2026-07-03",
+    home: "Argentina",
+    homeCode: "ar",
+    away: "Cabo Verde",
+    awayCode: "cv",
+    stadium: "Por confirmar"
+  },
+  {
+    id: 15,
+    date: "2026-07-03",
+    home: "Colombia",
+    homeCode: "co",
+    away: "Ghana",
+    awayCode: "gh",
+    stadium: "Por confirmar"
+  }
+];
